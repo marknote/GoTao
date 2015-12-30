dddff9967bbd804909f7499f754a18976a2526df
makenote <marknote@aliyun.com>
2015-12-30 22:40:06 +0800

    show moves

--- GoTao/GoTao/Game/GameViewController.swift
+++ GoTao/GoTao/Game/GameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
     @IBOutlet weak var _lblTitle:UILabel!
     @IBOutlet weak var _goban:GobanView!
     
+    var _currentMove = 0
+    
     var _game:GameInfo = GameInfo()
     override func viewDidLoad() {
         
@@ -21,13 +23,37 @@ import UIKit
             do {
                 let kifu = try NSString(contentsOfFile: filepath, encoding: NSUTF8StringEncoding)
                 _game = SGFParser().parse(kifu as String)
-                _lblTitle.text = "\(_game.playerBlack) v \(_game.playerWhite)"
-                _goban.moves = _game.allMoves
                 
+                //_goban.moves = _game.allMoves
+                showMoves()
                 
             }
             catch {/* error handling here */}
         }
-
     }
+    
+    @IBAction func backClick(sender:UIBarItem){
+        _currentMove -= 1
+        if (_currentMove < 0){
+            _currentMove = 0
+        }
+        showMoves()
+    }
+    
+    @IBAction func forwardClick(sender:UIBarItem){
+        _currentMove += 1
+        if (_currentMove > _game.allMoves.count - 1){
+            _currentMove = _game.allMoves.count - 1
+        }
+        showMoves()
+    }
+    func showMoves(){
+        if _game.allMoves.count > 0 {
+            let moves = _game.allMoves[0..<_currentMove]
+            _goban.moves = Array(moves)
+            _goban.setNeedsDisplay()
+            _lblTitle.text = "\(_game.playerBlack) v \(_game.playerWhite)   \(_currentMove + 1) / \(_game.allMoves.count)"
+        }
+    }
+
 }
