eb573ce930fc76f6c496aaf67e6da7eeab813ec0
makenote <marknote@aliyun.com>
2015-12-30 21:58:20 +0800

    load kifu and draw moves

--- GoTao/GoTao/Game/GameViewController.swift
+++ GoTao/GoTao/Game/GameViewController.swift
@@ -7,5 +7,27 @@
 import UIKit
 
  class GameViewController: UIViewController {
+    @IBOutlet weak var _lblTitle:UILabel!
+    @IBOutlet weak var _goban:GobanView!
+    
+    var _game:GameInfo = GameInfo()
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        if let filepath = NSBundle.mainBundle().pathForResource("001", ofType: "sgf") {
+    
+            //reading
+            do {
+                let kifu = try NSString(contentsOfFile: filepath, encoding: NSUTF8StringEncoding)
+                _game = SGFParser().parse(kifu as String)
+                _lblTitle.text = "\(_game.playerBlack) v \(_game.playerWhite)"
+                _goban.moves = _game.allMoves
+                
+                
+            }
+            catch {/* error handling here */}
+        }
 
+    }
 }
--- GoTao/GoTao/Game/GobanView.swift
+++ GoTao/GoTao/Game/GobanView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class GobanView: UIView {
+    var moves:[Move]?
 
     override func drawRect(rect: CGRect) {
         
@@ -18,11 +19,19 @@ class GobanView: UIView {
         let background = UIImage(named:"board_back")
         
         CGContextDrawImage(ctx, rec, background!.CGImage )
-        //draw lines
+        let space = w/20.0
+        drawLines(ctx!,space:space,w:w)
+        drawDots(ctx!,space:space)
+        if (moves != nil  && moves?.count > 0)  {
+            drawMoves(ctx!, stoneSize: space)
+        }
+        
+    }
+    func drawLines(ctx:CGContext,space:CGFloat, w:CGFloat){
         CGContextSetRGBStrokeColor(ctx, 0, 0, 0, 1)
         CGContextSetLineWidth(ctx, 0.4)
         CGContextBeginPath(ctx)
-        let space = w/20.0
+        
         for(var i=0;i<=18;i++)
         {
             
@@ -37,10 +46,9 @@ class GobanView: UIView {
             CGContextAddLineToPoint(ctx,w-space, (CGFloat(i+1))*space);
             CGContextStrokePath(ctx);
         }
-        //draw labels
-        
-        // nine dots
-        
+
+    }
+    func drawDots(ctx:CGContext,space:CGFloat){
         for (var i=0;i<=2;i++)
         {
             for(var j=0;j<=2;j++)
@@ -50,6 +58,28 @@ class GobanView: UIView {
                 CGContextAddArc(ctx, (CGFloat(1+3+6*i))*space ,CGFloat(1+3+6*j)*space, 2, 0, CGFloat(2.0*M_PI), 1);
                 
                 CGContextStrokePath(ctx);
+            }
+        }
+
+    }
+    
+    func drawMoves(ctx:CGContext,stoneSize:CGFloat ){
+        // chess
+        let imgBlack = UIImage(named: "Black.png")
+        let imgWhite = UIImage(named: "White.png")
+       
+        let  count = moves?.count
+        for (var i = 0; i < count; i++ )
+        {
+            let move = moves![i]
+            let imageRect = CGRectMake( (CGFloat(move.location.x)+0.5)*stoneSize,(CGFloat(move.location.y)+0.5)*stoneSize, stoneSize, stoneSize);
+            if move.type == .White
+            {
+                CGContextDrawImage(ctx, imageRect, imgWhite?.CGImage)
+            }else
+            {
+                CGContextDrawImage(ctx, imageRect, imgBlack?.CGImage )
+                
             }
         }
     }
